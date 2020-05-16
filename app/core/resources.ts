7c8185a41c05c719e62041c8aa14ec94affd8791
Vrishtrix <vrishin_patel@outlook.com>
2020-05-16 17:40:13 +0530

    [app] Designed loading screen

--- app/core/resources.ts
+++ app/core/resources.ts
@@ -3,8 +3,7 @@ import * as Font from 'expo-font'
 export const loadResourcesAsync = async() => {
 	await Promise.all([
 		Font.loadAsync({
-			'roboto-regular': require('../assets/fonts/roboto-regular.ttf'),
-			'balsamiq-sans-regular': require('../assets/fonts/balsamiq-sans-regular.ttf')
+			'museo-sans-regular': require('../assets/fonts/museo-sans-regular.ttf')
 		})
 	])
 }
