b664de1707c94cc75a39a7855064c701fb870593
Vrishtrix <vrishin_patel@outlook.com>
2020-05-17 11:55:23 +0530

    [app] Created landing screen (incomplete)

--- app/App.tsx
+++ app/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
-import { AppLoading } from 'expo'
+import { AppLoading } from 'expo';
 import { loadResourcesAsync, handleLoadingError } from './core/resources'
 
 //Screens
+import { LoadingScreen } from './Screens/LoadingScreen';
+import { LandingScreen } from './Screens/LandingScreen';
+
 
 export default function App() {
 
@@ -12,16 +15,19 @@ export default function App() {
   
   if (!isLoadingComplete) {
         return (
+            <>
+            <LoadingScreen />
             <AppLoading
                 startAsync={loadResourcesAsync}
                 onError={handleLoadingError}
                 onFinish={() => setLoadingComplete(true)}
             />
+            </>
         )
   }
   return (
     <View style={styles.container}>
-
+      <LandingScreen />
     </View>
   );
 }
@@ -29,6 +35,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    height: '100%',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
