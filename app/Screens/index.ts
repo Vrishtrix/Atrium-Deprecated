10ad769ede964d7a1e282d1bfb2065bb7bd5a743
Siddharth <siddharth1912@outlook.com>
2020-05-24 19:47:34 +0530

    updated server branch

--- app/Screens/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './DashScreen';
-export * from './LandingScreen';
-export * from './LoadingScreen';
-export * from './LoginScreen';
-export * from './VerifyScreen';
-export * from './RegisterScreen';
