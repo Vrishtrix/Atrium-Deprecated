f18629d2fcd769cd3661033379e403601c5a1173
Siddharth <siddharth1912@outlook.com>
2020-05-24 19:34:52 +0530

    [server] added try/catch

--- server/actions/modules.js
+++ server/actions/modules.js
@@ -7,44 +7,58 @@ const privateKEY = fs.readFileSync('./keys/private.key', 'utf8');
 const messagebird = require('messagebird')(process.env.API)
 
 module.exports.checkphone = async (phone) => {
-    const otp = otpGen.generate(6, { upperCase: false, specialChars: false, alphabets: false });
-    const hash = otpTool.createNewOTP(phone, otp, key);
-    this.sendotp(otp,phone)
-    return ({
-        "status": true,
-        "code":100,
-        "token": hash
-    })
-    
+    try {
+        const otp = otpGen.generate(6, { upperCase: false, specialChars: false, alphabets: false });
+        const hash = otpTool.createNewOTP(phone, otp, key);
+        this.sendotp(otp, phone)
+        return ({
+            "status": true,
+            "code": 100,
+            "token": hash
+        })
 
+    }
+    catch (err) {
+        return ("Server error")
+    }
 
 }
 
 
 module.exports.tokencreate = async (id, firstname, lastname, phone) => {
+    try {
 
-    const data = { id, firstname, lastname, phone }
-    const signOptions = { issuer: 'Atrium', subject: 'Login', audience: 'atrium.com', expiresIn: "48h", algorithm: "RS256" };
-    token = await jwt.sign(data, privateKEY, signOptions)
-    return ({
-        status: true,
-        "code":100,
-        token: token
-    })
+        const data = { id, firstname, lastname, phone }
+        const signOptions = { issuer: 'Atrium', subject: 'Login', audience: 'atrium.com', expiresIn: "48h", algorithm: "RS256" };
+        token = await jwt.sign(data, privateKEY, signOptions)
+        return ({
+            status: true,
+            "code": 100,
+            token: token
+        })
+    }
+    catch (err) {
+        return ("Server error")
+    }
 }
 
-module.exports.sendotp = async (otp , phone) =>{
-    var params = {
-        'originator': 'Atrium',
-        'recipients': [
-          `+91` + phone
-        ],
-        'body': `Your OTP for logging into Atrium is ${otp}`
+module.exports.sendotp = async (otp, phone) => {
+    try {
+        var params = {
+            'originator': 'Atrium',
+            'recipients': [
+                `+91` + phone
+            ],
+            'body': `Your OTP for logging into Atrium is ${otp}`
 
-      };
+        };
 
-      await messagebird.messages.create(params, async (err, response) => {
-         console.log(response)
-      });
+        await messagebird.messages.create(params, async (err, response) => {
+            console.log(response)
+        });
+    }
+    catch (err) {
+        return ("Server error")
+    }
 }
 
