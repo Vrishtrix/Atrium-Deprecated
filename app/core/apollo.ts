8520199057bf4134f83cb6d1345d48ca73b431a4
Siddharth <siddharth1912@outlook.com>
2020-05-27 15:37:08 +0530

    [app] changed apollo endpoint to azure functions

--- app/core/apollo.ts
+++ app/core/apollo.ts
@@ -1,5 +1,6 @@
 import ApolloClient from 'apollo-boost';
 
+
 export const client = new ApolloClient({
-      uri: 'http://localhost/graphql'
+      uri: "https://atrium-server.azurewebsites.net/api/graphql?code=rkLdgPYlpc4kgKftcqpF8uZ3q2luvNgTDzE192bTLUinKCetKdujGQ=="
 });
