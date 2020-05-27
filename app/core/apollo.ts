6ac39492ab824496887e9604490158c470f27132
Siddharth <siddharth1912@outlook.com>
2020-05-27 15:42:33 +0530

    Revert "[app] changed apollo endpoint to azure functions"
    This reverts commit 8520199057bf4134f83cb6d1345d48ca73b431a4.

--- app/core/apollo.ts
+++ app/core/apollo.ts
@@ -1,6 +1,5 @@
 import ApolloClient from 'apollo-boost';
 
-
 export const client = new ApolloClient({
-      uri: "https://atrium-server.azurewebsites.net/api/graphql?code=rkLdgPYlpc4kgKftcqpF8uZ3q2luvNgTDzE192bTLUinKCetKdujGQ=="
+      uri: 'http://localhost/graphql'
 });
